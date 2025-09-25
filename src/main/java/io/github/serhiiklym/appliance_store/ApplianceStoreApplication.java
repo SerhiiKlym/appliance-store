246efa54e84baaa369234bd466db9e70e86bc584
SerhiiKlymenko <mafakka@mail.ru>
2025-09-25 17:13:28 -0400

    chore: import Spring Initializr project (appliance-store)
    - Spring Boot 3.5.6, Java 21, packaging=jar
    - Dependencies: Web, Thymeleaf, Data JPA, Validation, H2, Lombok, Security, Actuator
    - Commit Maven Wrapper + .gitattributes for correct line endings
    - Add ApplianceStoreApplication and initial test
    - Verify app boots locally on :8080

---
+++ src/main/java/io/github/serhiiklym/appliance_store/ApplianceStoreApplication.java
@@ -0,0 +1,13 @@
+package io.github.serhiiklym.appliance_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplianceStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplianceStoreApplication.class, args);
+	}
+
+}
