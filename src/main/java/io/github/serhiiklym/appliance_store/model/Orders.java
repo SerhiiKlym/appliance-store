86fe111c15b67a134db6eb7a442459b33b6f7acd
SerhiiKlymenko <mafakka@mail.ru>
2025-09-26 10:04:10 -0400

    model - Orders

--- src/main/java/io/github/serhiiklym/appliance_store/model/Orders.java
+++ src/main/java/io/github/serhiiklym/appliance_store/model/Orders.java
@@ -1,5 +1,42 @@
 package io.github.serhiiklym.appliance_store.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "ORDERS")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Orders {
+
+    @OneToMany()
+    private Set<OrderRow> orderRowSet = new HashSet<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "EMPLOYEE")
+    private Employee employee;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "CLIENT")
+    @NotNull
+    private Client client;
+
+    @Column(name = "APPROVED")
+    @NotNull
+    private Boolean approved;
+
+    @Id
+    @Column(name = "ORDER_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
 }
