1ac2e7d796f4973e8b95039f3b1d8ec6599577be
SerhiiKlymenko <mafakka@mail.ru>
2025-09-26 10:02:38 -0400

    model - Client

--- src/main/java/io/github/serhiiklym/appliance_store/model/Client.java
+++ src/main/java/io/github/serhiiklym/appliance_store/model/Client.java
@@ -1,5 +1,29 @@
 package io.github.serhiiklym.appliance_store.model;
 
-public class Client {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Entity
+@Table(name = "CLIENT")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Client extends User {
+
+    @Autowired
+    public Client(Long id, String name, String email, String password, String card) {
+        super(id, name, email, password);
+        this.card = card;
+    }
+
+    @Column(name = "CARD")
+    @NotBlank
+    private String card;
 
 }
