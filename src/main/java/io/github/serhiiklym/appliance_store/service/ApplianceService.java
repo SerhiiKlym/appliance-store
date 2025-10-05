36d324d11ad32d296f73b57e55a486426250f718
SerhiiKlymenko <mafakka@mail.ru>
2025-10-04 20:15:39 -0400

    feat: appliance slice - interface

--- src/main/java/io/github/serhiiklym/appliance_store/service/ApplianceService.java
+++ src/main/java/io/github/serhiiklym/appliance_store/service/ApplianceService.java
@@ -1,4 +1,23 @@
 package io.github.serhiiklym.appliance_store.service;
 
+import io.github.serhiiklym.appliance_store.model.Appliance;
+import io.github.serhiiklym.appliance_store.model.Category;
+import io.github.serhiiklym.appliance_store.model.PowerType;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface ApplianceService {
+
+    List<Appliance> list();
+
+    Appliance getByIdOrThrow(Long id);
+
+    Appliance create(String name, Category cat, String model, Long manufacturerId, PowerType powerType,
+                     String props, String descr, Integer power, BigDecimal price);
+
+    Appliance update(Long id, String name, Category cat, String model, Long manufacturerId, PowerType powerType,
+                     String props, String descr, Integer power, BigDecimal price);
+
+    void deleteAppliance(Long id);
 }
